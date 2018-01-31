96536a7ff0b5b88ad155ec570872bf60ae2ff9ed
TauWoo <tauwoo@seuxw.cc>
2018-01-31 12:49:04 +0800

    IP代理池生成器代码

--- tool/proxies_holder/proxies_holder.py
+++ tool/proxies_holder/proxies_holder.py
@@ -12,21 +12,32 @@ import time
 # ------------
 
 class IPProxies():
+    """
+    IP代理抓取模块
 
-    def __init__(self, page):
-        self.url = "http://www.xicidaili.com/nn/{page}".format(page=page)
-        self.headers = {
+    可访问成员（函数）：
+    - start
+
+    """
+
+    def __init__(self, start=1, end=2):
+        #TODO 参数检查
+        self.__raw_url__ = "http://www.xicidaili.com/nn/{page}"
+        self.__start__ = start
+        self.__end__ = end
+        self.__headers__ = {
             "Accecpt":"text/html,application/xhtml+xml,application/xml",  
             "Accept-Encoding":"gzip,deflate,sdch",  
             "Accept-Language":"zh-CN,zh;q=0.8,en;q=0.6",  
             "Referer":"http://www.xicidaili.com",  
             "User-Agent":"Mozilla/5.0(Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36"  
         }
+        self.db = IPDBController()
 
-    @property
-    def _get_ip(self):
+    def __get_ip__(self, page):
         # 获取代理IP池
-        req = requests.get(self.url, headers=self.headers, timeout=1)
+        url = self.__raw_url__.format(page=page)
+        req = requests.get(url, headers=self.__headers__, timeout=1)
         bs = bs4.BeautifulSoup(req.text, "lxml")
         data = bs.find_all("td")
         
@@ -49,26 +60,34 @@ class IPProxies():
         return ip_list
 
     @property
-    def get_all(self):
-        ip_lists = self._get_ip
-        yield [ip_list.split(',') for ip_list in ip_lists]
+    def ip_port_list(self):
+        for page in range(self.__start__, self.__end__):
+            ip_lists = self.__get_ip__(page)
+            # 返回一页的URL数据
+            yield [ip_list.split(',') for ip_list in ip_lists]
 
     @property
-    def get_ip(self):
-        ip_lists = self._get_ip
-        yield [ip_list.split(',')[0] for ip_list in ip_lists]
-
-    @property
-    def get_ip_port(self):
-        ip_lists = self._get_ip
-        yield [":".join(ip_list.split(',')[0:2]) for ip_list in ip_lists]
-        
+    def ip_list(self):
+        for page in range(self.__start__, self.__end__):
+            ip_lists = self.__get_ip__(page)
+            # 返回一页的URL数据
+            yield [ip_list.split(',')[0] for ip_list in ip_lists]  
 
 # ------------
 # 数据库操作模块
 # ------------
 
 class DBController():
+    """
+    数据库操作模块
+    
+    可访问成员（函数）：
+    - cur
+    - IntegrityError
+    - execute(sql)
+    - close
+
+    """
 
     def __init__(self, host="localhost", user="root", passwd="root", port=3306, db="spider_data"):
         import pymysql
@@ -91,31 +110,59 @@ class DBController():
         self.cur.close()
 
 
-# ------------
-# 数据库操作模块
-# ------------
+class IPDBController(DBController):
+    """
+    IP代理数据库操作模块
 
-if __name__ == "__main__":
-    db = DBController()
+    可访问成员（函数）：
+    - insert_ip(ip_port_list)
 
-    sql_template = """
-    insert into
-        proxies_pool(ip, port, type)
-    values
-        ("%s", %s, "%s")
     """
 
-    for page in range(1,10):
-        IP = IPProxies(page)
-        ip_list = next(IP.get_all)
-        for ip in ip_list:
-            sql = sql_template%(ip[0],ip[1],ip[2])
+    def __init__(self):
+    # 初始化IPDBController对象
+        DBController.__init__(self)
+        self.__insert_sql_template__ = """
+        insert into
+            proxies_pool(ip, port, type)
+        values
+            ("{ip}", {port}, "{type}")
+        """
+
+    def insert_ip(self, ip_port_list):
+    # 向IP数据库中插入一页的IP代理数据
+        for ip_port in ip_port_list:
+            print(ip_port)
+            (ip, port, _type) = tuple(ip_port)
+            insert_sql = self.__insert_sql_template__.format(ip=ip, port=port, type=_type)
             try:
-                db.execute(sql)
-                print("插入IP【%s】成功"%ip[0])
-            except db.IntegrityError:
-                print("忽略插入重复【%s】"%"-".join(ip))
+                DBController.execute(self, insert_sql)
+                print("插入IP【%s】成功"%ip)
+            except self.IntegrityError:
+                print("忽略插入重复【%s】"%"-".join(ip_port))
                 pass
-        time.sleep(5)
+
+        DBController.close
+
+# ------------
+# IP代理检查模块
+# 通过访问ip138验证
+# ------------
+# class IPVerify():
+
+#     def __init__(self, timeout=1, ):
+
+if __name__ == "__main__":
+    # 创建一个插入IP代理的对象
+    db = IPDBController()
+
+    # 创建一个生成IP代理的对象
+    IP = IPProxies(start=1,end=4)
     
-    db.close
+    ip_lists = IP.ip_port_list
+
+    for ip_lists in ip_lists:
+        ip_list = ip_lists
+        db.insert_ip(ip_list)
+        print("页面爬取请求延迟10s...")
+        time.sleep(10)
